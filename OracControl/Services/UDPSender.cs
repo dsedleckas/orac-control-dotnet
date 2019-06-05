aa040c6463fb05e3f2f82c3bb57f9fc1127c779a
dsedleckas <dominykas.sedleckas@gmail.com>
2019-06-06 01:35:27 +0300

    Try simpler UDP sender

---
+++ OracControl/Services/UDPSender.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace OracControl.Services
+{
+    public class UDPSender
+    {
+        public int Port { get; set; }
+        public IPAddress Address { get; set; }
+
+        IPEndPoint RemoteIpEndPoint;
+        Socket sock;
+
+        public UDPSender(IPAddress address, int port)
+        {
+            Port = port;
+            Address = address;
+
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+
+            RemoteIpEndPoint = new IPEndPoint(address, port);
+        }
+
+        public void Send(byte[] message)
+        {
+            sock.SendTo(message, RemoteIpEndPoint);
+        }
+
+        public void Close()
+        {
+            sock.Close();
+        }
+    }
+}
